198162a2c677c4dd55ff5331a858f73d51daa5b0
PicotDesBois <79858497+PicotDesBois@users.noreply.github.com>
2022-02-09 10:31:13 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int entier;
+    float reel;
+    std::cout << "Saisir un entier " << std::endl;
+    std::cin >> entier;
+    std::cout << "Saisir un réel "<<std::endl;
+    std::cin >> reel;
+    std::cout << std::endl << "Reel "<< reel << std::endl<< "Entier "<<entier;
+
+    return 0;
+}
